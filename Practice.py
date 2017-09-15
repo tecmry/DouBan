eeac7060eb4216a855525c18493abce909ed44d0
tecmry <tecmry6@gmail.com>
2017-09-15 08:44:39 +0800

    将能播放的电影链接以及评分爬取下来了

--- Practice.py
+++ Practice.py
@@ -13,7 +13,8 @@ DOWNLOAD_URL = 'http://movie.douban.com/top250/'
 
 def download_page(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_1'
+                      '+1_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
     }
     data = requests.get(url, headers=headers).content
 
@@ -24,64 +25,53 @@ def soup_html(html):
     movie_list_soup = soup.find('ol', attrs={'class': 'grid_view'})
     movie_name_list = []
     movie_common_list = []
+    movie_score_list = []
+    movie_link_list = []
     for movie_li in movie_list_soup.find_all('li'):
-        detail = movie_li.find('div', attrs={'class': 'hd'})
+        detail = movie_li.find('div', attrs={'class': 'hd'}).find('a')
         try:
+            movielink = detail['href']
             moviename = movie_li.find('span', attrs={'class': 'title'}).getText()
             commond = movie_li.find('span', attrs={'class': 'inq'}).getText()
+            score = movie_li.find('span', attrs={'class': 'rating_num'}).getText()
         except AttributeError:
             print('Null')
         movie_name_list.append(moviename)
         movie_common_list.append(commond)
+        movie_score_list.append(score)
+        movie_link_list.append(movielink)
     nextPage = soup.find('span', attrs={'class': 'next'}).find('a')
     if nextPage:
-        return movie_name_list, movie_common_list, DOWNLOAD_URL + nextPage['href']
-    return movie_name_list, movie_common_list , None
-
-def common_html(html):
-    soup = BeautifulSoup(html, 'lxml')
-    common_list_soup = soup.find('ol', attrs={'class': 'grid_view'})
-    common_list = []
-    for common_li in common_list_soup.find_all('li'):
-        detail = common_li.find('div', attrs={'class': 'hd'})
-        try:
-            commond = common_li.find('span', attrs={'class': 'inq'}).getText()
-        except AttributeError:
-            print('Null')
-        common_list.append(commond)
-    nextPage = soup.find('span', attrs={'class': 'next'}).find('a')
-    if nextPage:
-        return common_list, DOWNLOAD_URL + nextPage['href']
-    return common_list, None
-
-
-
+        return movie_name_list, movie_common_list, movie_score_list, movie_link_list, DOWNLOAD_URL + nextPage['href']
+    return movie_name_list, movie_common_list, movie_score_list, movie_link_list, None
 def main():
     url = DOWNLOAD_URL
     with codecs.open('movies', 'wb', encoding='utf-8') as fp:
         while url:
             html = download_page(url)
-            movies, common, url = soup_html(html)
-            data_list = add_dict(movies, common)
+            movies, common, score, link, url = soup_html(html)
+            data_list = add_dict(movies, common, score, link)
             save_data(data_list)
-            fp.write(u'{movies}\n'.format(movies='\n'.join(movies)))
+
 
 def save_data(dict_list):
     client = pymongo.MongoClient('localhost', 27017)
-    db_name = 'douban_movie'
+    db_name = 'douban_movie_2'
     db = client[db_name]
     collection_set01 = db['movie_common']
     try:
        collection_set01.insert(dict_list)
+
     except pymongo.errors.DuplicateKeyError:
         print('DuplicateKey')
     except Exception as e:
         print('e')
-def add_dict(movie_list, common_list):
+
+def add_dict(movie_list, common_list, score_list, link_list):
     dict_list = []
     for i in range(0, movie_list.__len__()):
-        list1 = ['Name', 'Common']
-        list2 = [movie_list[i], common_list[i]]
+        list1 = ['Name', 'Common', 'Score', 'link']
+        list2 = [movie_list[i], common_list[i], score_list[i], link_list[i]]
         dicti = dict(zip(list1, list2))
         dict_list.append(dicti)
     return dict_list
